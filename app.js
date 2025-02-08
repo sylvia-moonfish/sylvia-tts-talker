8c25c9add9ef5d6d29d049faa0495f74c4d3411c
korean-patch <sylvia.moonfish@gmail.com>
2025-02-08 10:53:20 -0800

    added split input

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ const DEFAULT_PROMPT_PATH = "GPT_SoVITS/pretrained_models/default_prompt.wav";
 const DEFAULT_PROMPT_TEXT = "그는 괜찮은 척하려고 애쓰는 것 같았다.";
 const DEFAULT_PROMPT_LANGUAGE = "all_ko";
 
+const PUNCTUATIONS = [
+  "，",
+  "。",
+  "？",
+  "！",
+  ",",
+  ".",
+  "?",
+  "!",
+  "~",
+  ":",
+  "：",
+  "—",
+  "…",
+];
+
 require("dotenv").config();
 
 const {
+  AudioPlayerStatus,
   createAudioPlayer,
   createAudioResource,
   joinVoiceChannel,
@@ -56,38 +73,120 @@ const client = new Client({
   ],
 });
 
-const player = createAudioPlayer();
+const guilds = {};
 
-async function streamAudioResponse(message) {
-  try {
-    const permissions = message.channel.permissionsFor(client.user);
+class GuildAudioManager {
+  constructor(guildId) {
+    this.guildId = guildId;
+    this.queue = [];
+    this.player = createAudioPlayer();
+    this.isPlaying = false;
 
-    if (
-      !permissions.has(PermissionsBitField.Flags.Connect) ||
-      !permissions.has(PermissionsBitField.Flags.Speak)
-    ) {
-      console.error("Permission error");
-      return;
+    this.player.on(AudioPlayerStatus.Idle, () => {
+      this.isPlaying = false;
+      this.playNext();
+    });
+
+    this.player.on("error", (error) => {
+      console.error(`Error in guild ${guildId}: `, error);
+
+      this.isPlaying = false;
+      this.playNext();
+    });
+  }
+
+  split(text) {
+    text = text
+      .trim()
+      .replaceAll("……", ".")
+      .replaceAll("——", ",")
+      .replaceAll("ㅋ", "크")
+      .replaceAll("ㅎ", "헤")
+      .replaceAll("ㅠ", "유");
+
+    if (!PUNCTUATIONS.includes(text[text.length - 1])) {
+      text += ".";
     }
 
-    const connection = joinVoiceChannel({
-      adapterCreator: message.channel.guild.voiceAdapterCreator,
-      channelId: message.channel.id,
-      guildId: message.channel.guild.id,
-      selfDead: false,
-      selfMute: false,
-    });
-    connection.subscribe(player);
+    const lines = [];
+    let chars = [];
 
+    for (let i = 0; i < text.length; i++) {
+      if (
+        i > 3 &&
+        text[i - 4] === text[i - 3] &&
+        text[i - 3] === text[i - 2] &&
+        text[i - 2] === text[i - 1] &&
+        text[i - 1] === text[i]
+      )
+        continue;
+
+      chars.push(text[i]);
+
+      if (PUNCTUATIONS.includes(text[i])) {
+        lines.push(chars.join("").trim());
+        chars = [];
+      }
+    }
+
+    return lines;
+  }
+
+  processMessage(message) {
     const userId = message.author.id;
+    const text = message.content;
+
+    const lines = this.split(text);
+    const results = [];
+    let curLine = "";
+
+    lines.forEach((line) => {
+      curLine += line;
+
+      if (curLine.length > 10) {
+        results.push({ userId, text: curLine });
+        curLine = "";
+      }
+    });
+
+    if (curLine.length > 0) results.push({ userId, text: curLine });
+
+    return results;
+  }
+
+  async addToQueue(message) {
+    const messages = this.processMessage(message);
+    messages.forEach((m) => this.queue.push(m));
+
+    if (!this.isPlaying) {
+      await this.playNext();
+    }
+  }
+
+  async playNext() {
+    if (this.queue.length === 0) return;
+
+    const message = this.queue.shift();
+    this.isPlaying = true;
+
+    try {
+      await this.playAudio(message);
+    } catch (error) {
+      console.error(`Error playing audio: `, error);
+
+      this.isPlaying = false;
+      this.playNext();
+    }
+  }
+
+  async playAudio(message) {
+    const userId = message.userId;
 
     let gptPath = DEFAULT_GPT_PATH;
     let sovitsPath = DEFAULT_SOVITS_PATH;
     let promptPath = DEFAULT_PROMPT_PATH;
     let promptText = DEFAULT_PROMPT_TEXT;
     let promptLanguage = DEFAULT_PROMPT_LANGUAGE;
-
-    loadData();
 
     if (userId in users) {
       const user = users[userId];
@@ -125,7 +224,7 @@ async function streamAudioResponse(message) {
         prompt_path: promptPath,
         prompt_text: promptText,
         sovits_path: sovitsPath,
-        text: message.content,
+        text: message.text,
         text_language: "auto",
       },
       responseType: "stream",
@@ -135,11 +234,53 @@ async function streamAudioResponse(message) {
     const resource = createAudioResource(response.data, {
       inputType: StreamType.Raw,
     });
-    resource.playStream.on("error", (error) =>
-      console.error("Play stream error: ", error)
-    );
 
-    player.play(resource);
+    resource.playStream.on("error", (error) => {
+      console.error("Play stream error: ", error);
+
+      this.isPlaying = false;
+      this.playNext();
+    });
+
+    this.player.play(resource);
+  }
+}
+
+async function streamAudioResponse(message) {
+  if (!message || !message.content || !message.author) return;
+  if (
+    message.content[0] === ":" &&
+    message.content[message.content.length - 1] === ":"
+  )
+    return;
+  if (message.content.startsWith("https:")) return;
+
+  try {
+    const permissions = message.channel.permissionsFor(client.user);
+
+    if (
+      !permissions.has(PermissionsBitField.Flags.Connect) ||
+      !permissions.has(PermissionsBitField.Flags.Speak)
+    ) {
+      console.error("Permission error");
+      return;
+    }
+
+    const connection = joinVoiceChannel({
+      adapterCreator: message.channel.guild.voiceAdapterCreator,
+      channelId: message.channel.id,
+      guildId: message.channel.guild.id,
+      selfDead: false,
+      selfMute: false,
+    });
+
+    const guildId = message.guild.id;
+
+    if (!guilds[guildId]) guilds[guildId] = new GuildAudioManager(guildId);
+
+    connection.subscribe(guilds[guildId].player);
+
+    await guilds[guildId].addToQueue(message);
   } catch (error) {
     console.error("Error streaming audio: ", error);
   }
